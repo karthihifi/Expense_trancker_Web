6e7eed65ffc51c9eb7ccc893a7845fc950c1b4cf
karthihifi <karthi.hifi@gmail.com>
2022-08-13 00:40:08 +0530

    dada

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/main'
 import Login from './components/login'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Link,
+  useParams
+} from "react-router-dom";
 
 // function App() {
-  const App : React.FC = () =>{
+const App: React.FC = () => {
+
+  const [Username, setUsername] = React.useState('');
+
   return (
-    <div className="App">
-      {/* <Main></Main> */}
-      <Login></Login>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login Username={Username} setUsername={setUsername} />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+{/* <div className="App">
+      <Main></Main> 
+       <Login></Login> 
+     </div>  */}
