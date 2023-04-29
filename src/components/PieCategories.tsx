d15f9399b3453fb99ad3168ceff9d0a01fb7455a
karthihifi <karthi.hifi@gmail.com>
2023-04-29 15:29:56 +0800

    Added Category

--- src/components/PieCategories.tsx
+++ src/components/PieCategories.tsx
@@ -113,7 +113,7 @@ class PieCat {
             key: 'Food', value: ['Snacks', 'Beverages', 'MainCourse', 'Fruits', 'Nuts', 'Water', 'BreadItems', 'Pasteries', 'Chinese', 'NewTryOuts']
         },
         {
-            key: 'Groceries', value: ['Fish', 'Chicken', 'Lamb', 'Prawn', 'Egg', 'Flour', 'Nuts', 'Salt', 'Sugar', 'Dhal', 'Rava', 'OtherMeat', 'Vegetables', 'CookingOil', 'OtherOil', 'Friuits', 'DosaBatter', 'Milk', 'Yogurt', 'Spices', 'Masala', 'Rice', 'Wheat']
+            key: 'Groceries', value: ['Fish', 'Chicken', 'Lamb', 'Prawn', 'Egg', 'Flour', 'Nuts', 'Salt', 'Sugar', 'Dhal', 'Rava', 'OtherMeat', 'Vegetables', 'CookingOil', 'OtherOil', 'Friuits', 'DosaBatter', 'Milk', 'Yogurt', 'Spices', 'Masala', 'Rice', 'Basmati Rice', 'Wheat', 'CoffePowder', 'Milo']
         },
         {
             key: 'Travel', value: ['Train', 'Bus', 'Car', 'Bike', 'Taxi']
